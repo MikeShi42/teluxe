b4095646221cbbf57f800aafec32ab4f0df96bf3
Mike Shi <michaelshi42@gmail.com>
2014-10-05 10:24:03 -0700

    -changed fb href

--- stats/stats.js
+++ stats/stats.js
@@ -39,7 +39,7 @@ angular.module("teluxe")
             FB.ui(
                 {
                     method: 'share',
-                    href: 'teluxe.me',
+                    href: 'http://mikeshi42.github.io/teluxe',
                     caption:$scope.shareText
                 },
                 function(response) {
